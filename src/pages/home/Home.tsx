63e8db4cb5d83e126e9b3ae137f8f01d32bdea0c
unknown <jaimebrancofilho@gmail.com>
2025-02-17 10:08:13 -0300

    segundo commit

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,38 +1,25 @@
 function Home() {
     return (
-        <>
-            <div style={{
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center"
-            }}>
-                <div>
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
-                        <h2>Seja Bem Vinde!</h2>
-                        <p>Expresse aqui seus pensamentos e opniões</p>
-                    </div>
-
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
-                        <img 
-                            src="https://i.imgur.com/VpwApCU.png" 
-                            alt="Imagem da Página Home" 
-                            width="400px"
-                        />
-                    </div>
-                </div>
+      <>
+        <div className="w-screen flex justify-center">
+          <div>
+            <div className="max-w-7xl flex flex-col items-center">
+              <h2>Seja Bem Vinde!</h2>
+              <p>Expresse aqui seus pensamentos e opiniões</p>
             </div>
-        </>
-    )
-}
-
-export default Home
+  
+            <div className="max-w-7xl flex flex-col items-center justify-center">
+              <img
+                src="https://i.imgur.com/VpwApCU.png"
+                alt="Imagem da Página Home"
+                width="400px"
+                className="mx-auto"
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default Home;
