5c9ef709a170e6800584d44821952e37a1cdf764
unknown <jaimebrancofilho@gmail.com>
2025-03-07 00:38:48 -0300

    toast alert

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 ﻿import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://blogpessoal.onrender.com'
+    baseURL: 'https://blogpessoal-qdax.onrender.com'
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) =>{
